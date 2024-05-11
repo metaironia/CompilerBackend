2473d2db93332a4203be23bb13390193c6668bc0
metaironia <serezha364@gmail.com>
2024-05-11 16:13:33 +0300

    added first version of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,45 @@
 #include "../lib/tree/math_tree/dsl_commands.h"
 #include "../lib/tree/math_tree/math_tree_func.h"
 
+#include "backend_input.h"
+
 #include "ASTtoIR/ast_to_ir.h"
+#include "ASTtoIR/ir_log.h"
 
 int main (const int argc, const char *argv[]) {
 
-    IntRepr interm_repr = {};
+    if (BackendCmdArgsCheck (argc) == BACKEND_FUNC_STATUS_FAIL)
+        return -1;
 
+    FILE *input_file = fopen (InputFileName (argv), "r");
+
+    Tree lang_tree = {};
+    TreeCtor (&lang_tree);
+
+    NameTable lang_name_table = {};
+    NameTableCtor (&lang_name_table);
+
+    LangTreeNodeRead (input_file, &(lang_tree.root), &lang_name_table);
+    assert (lang_tree.root);
+
+    MathTreeGraphDump (&lang_tree, &lang_name_table);
+
+    fclose (input_file);
+    input_file = NULL;
+
+    LangTreeVarsSet (&lang_tree, &lang_name_table);
+
+    IntRepr interm_repr = {};
     IntReprCtor (&interm_repr);
-    IntReprDtor (&interm_repr);
+
+int mem_disp = 0; //TODO put to another file
+
+    IntReprMathExpressionWrite (&interm_repr, lang_tree.root, &mem_disp);    
+    IntReprDump (&interm_repr);
+
+    TreeDtor      (&lang_tree);
+    NameTableDtor (&lang_name_table);
+    IntReprDtor   (&interm_repr);
 
     return 0;
 }
